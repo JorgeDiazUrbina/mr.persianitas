acea8da3111f208c3fb790ffcf2755bb1470aab6
JorgeDiazUrbina <40496145+JorgeDiazUrbina@users.noreply.github.com>
2019-07-16 00:11:57 -0500

    primer commit

---
+++ view/inicio_sesion.php
@@ -0,0 +1,60 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mr persianas</title>
+   <!--Made with love by Mutiullah Samim -->
+   
+	<!--Bootsrap 4 CDN-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <!--Fontawesome CDN-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+	<!--Custom styles-->
+	<link rel="stylesheet" type="text/css" href="../css/ini_ses.css">
+</head>
+<body>
+
+    
+<div class="container">
+	<div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h3>Administrador</h3>
+				<div class="d-flex justify-content-end social_icon">
+					<span><a  href="https://www.facebook.com/mrpersianas2015/?ref=br_rs" ><i class="fab fa-facebook-square facebook_fa"></i></a></span>
+				</div>
+			</div>
+			<div class="card-body">
+				<form>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+						<input type="text" class="form-control" placeholder="usuario">
+						
+					</div>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+						<input type="password" class="form-control" placeholder="contraseña">
+					</div>
+                    <div class="form-group">
+						<input type="submit" value="Volver" class="btn float-left login_btn">
+					</div>
+					<div class="form-group">
+						<input type="submit" value="Iniciar sesión" class="btn float-right login_btn">
+					</div>
+				</form>
+			</div>
+        </div>
+	</div>
+</div>
+</body>
+</html>
